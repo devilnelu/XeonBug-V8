20789208ea4f65e29c0f0e76a5bbea2c1aaf83f6
devilnelu <chutisose@gmail.com>
2024-10-03 13:47:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "649107894179"
+global.ownernomer = "639107894179"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '649107894179'  //creator number
+global.ownernumber = '639107894179'  //creator number
 global.ownername = 'Qasim Ali' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
